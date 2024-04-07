1f2a148ae872fcf9e9e48721bf68c169f6e00911
ciroluiz <ciroluiz.cl@gmail.com>
2024-04-07 18:15:58 -0300

    Integrando com as APIs OPENAI e GEMINI

--- src/main/java/me/dio/sdw24/application/AskChampionUseCase.java
+++ src/main/java/me/dio/sdw24/application/AskChampionUseCase.java
@@ -3,19 +3,22 @@ package me.dio.sdw24.application;
 import me.dio.sdw24.domain.exception.ChampionNotFoundException;
 import me.dio.sdw24.domain.model.Champion;
 import me.dio.sdw24.domain.ports.ChampionsRepository;
+import me.dio.sdw24.domain.ports.GenerativeAiService;
 
-import java.util.List;
-
-public record AskChampionUseCase(ChampionsRepository repository) {
+public record AskChampionUseCase(ChampionsRepository repository, GenerativeAiService genAiService) {
 
     public String askChampion(Long championId, String question) {
         Champion champion = repository.findById(championId)
                 .orElseThrow(() -> new ChampionNotFoundException(championId));
 
-        String championContext = champion.generateContextByQuestion(question);
+        String context = champion.generateContextByQuestion(question);
+        String objective = """
+                Atue como um assistente com a habilidade de se comportar como os campeões do League of Legends(LoL).
+                Responda perguntas incorporando a personalidade de um determinado Campeão.
+                Segue a pergunta, o nome do Campeão e sua respectiva lore(história):
+                                
+                """;
 
-        // TODO: Evoluir a lógica de negócio para considerar a integração com IAs generativas
-
-        return championContext;
+        return genAiService.generateContent(objective, context);
     }
 }
